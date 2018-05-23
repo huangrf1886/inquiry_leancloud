62c6ca30a1f040e5b869dee7efd26bb90f6d9072
huangrf1886 <huangrf1886@sina.cn>
2018-05-23 09:57:24 +0800

    initialize

---
+++ js/init.js
@@ -0,0 +1,8 @@
+			var Realtime = AV.Realtime;
+			var TextMessage = AV.TextMessage;
+			
+			var realtime = new Realtime({
+			  appId: 'SHucafGuXDB4YFspqvQLwtU5-gzGzoHsz',
+			  appKey: 'ou6Ky6GnI0eG2ckou4MaBFSw',
+			  region: 'cn', // 美国节点为 "us"
+			});
